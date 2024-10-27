583d05568d0b95aba9df45fad573cc9fbf150e15
sdengz <221991654@edu.vut.ac.za>
2024-10-27 13:32:09 +0200

    Update application.py

--- application.py
+++ application.py
@@ -282,3 +282,56 @@ def trending_courses():
     trending_html = trending_courses_analysis()
     return render_template('trending_courses.html', trending_html=trending_html)
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as b:
+    intents = json.load(b)
+
+FILE = "data.pth"
+data = torch.load(FILE, map_location=device, weights_only=True)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data["all_words"]
+tags = data["tags"]
+model_state = data["model_state"]
+
+model = Chat(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "ZEN"
+
+@app.route('/bot')
+def chatb():
+    return render_template('chatbot.html')
+    
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.json['message']
+    
+    sentence = tokenize(user_input)
+    x = words(sentence, all_words)
+    x = torch.from_numpy(x).reshape(1, x.shape[0]).to(device)
+    
+    output = model(x)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent['tag']:
+                response = random.choice(intent['responses'])
+                return jsonify({"response": response})
+    else:
+        return jsonify({"response": f"{bot_name}: I do not understand..."})
+
+
+if name == 'main':
+    app.run(debug=True)
+
